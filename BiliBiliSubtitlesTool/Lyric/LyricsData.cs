1d6a2a6eb891317145d95dd78577c269bfdb4ab5
millennium <1543255825@qq.com>
2022-03-29 22:02:39 +0800

    初步完成歌词的存储

---
+++ BiliBiliSubtitlesTool/Lyric/LyricsData.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BiliBiliSubtitlesTool.Lyric
+{
+    public class LyricsData : ICollection<(TimeTag, string)>, ICloneable
+    {
+        /// <summary>
+        /// 结束标志
+        /// </summary>
+        public const string END_FLAG = "-----END-----";
+        private readonly LinkedList<(TimeTag timeTag, string lyrics)> _data =
+            new LinkedList<(TimeTag timeTag, string lyrics)>();
+        public int Count => _data.Count;
+        /// <summary>
+        /// 歌曲时长(单位为毫秒)
+        /// </summary>
+        public long SongTime;
+        //private UnifiedLanguageCode? _contentLanguage = null;
+
+        /// <summary>
+        /// 集合中每个元素为LRC文件中的一行
+        /// </summary>
+        /// <param name="lrcFileRawData"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public LyricsData(IEnumerable<string> lrcFileRawData)
+        {
+            if (lrcFileRawData == null)
+                throw new ArgumentNullException(nameof(lrcFileRawData));
+
+            var list = new List<string>();
+            foreach (var item in lrcFileRawData)
+            {
+                var result = GetTimeTagAndLyrics(item);
+                list.Add(result.lyrics);
+                _data.AddLast(result);
+            }
+        }
+      
+        private static (TimeTag timeTag, string lyrics) GetTimeTagAndLyrics(string rawLine)
+        {
+            (TimeTag timeTag, string lyrics) lineData;
+            lineData.timeTag = new TimeTag(rawLine);
+            lineData.lyrics = Tools.GetLineLyric(rawLine);
+            return lineData;
+        }
+
+        /// <summary>
+        /// 翻译到指定语种
+        /// </summary>
+        /// <param name="api">翻译API</param>
+        /// <param name="targetLanguage">翻译到的语言</param>
+        /// <returns>一个新的<see cref="LyricsFile"/>对象</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        //public LyricsFile TranslateTo(ITranslation api, UnifiedLanguageCode targetLanguage)
+        //{
+        //    if (api == null)
+        //        throw new ArgumentNullException(nameof(api));
+
+        //    LyricsFile newLyricsFile = (LyricsFile)((ICloneable)this).Clone();
+        //    newLyricsFile._contentLanguage = targetLanguage;
+
+        //    string rawLyrscs = GetAllLyrics();
+        //    string correspondingLanguageCode = api.GetStandardTranslationLanguageParameters(targetLanguage);
+        //    var translation = api.GetTransResultAndRawDataMap(rawLyrscs, "auto", correspondingLanguageCode);
+        //    foreach (string rawLyrics in translation.Keys)
+        //    {
+        //        for (var node = newLyricsFile._data.First; node != null; node = node.Next)
+        //        {
+        //            if (node.Value.lyrics == rawLyrics)
+        //            {
+        //                node.Value = (node.Value.timeTag, translation[rawLyrics]);
+        //            }
+        //        }
+        //    }
+        //    return newLyricsFile;
+        //}
+
+
+        /// <summary>
+        /// 得到data里的所有lyrics
+        /// </summary>
+        /// <returns>data里的所有lyrics组成的一个字符串</returns>
+        //private string GetAllLyrics()
+        //{
+        //    List<string> lyrics = new List<string>(_data.Count);
+        //    foreach (var item in _data)
+        //    {
+        //        lyrics.Add(item.lyrics);
+        //    }
+        //    return LyricsTool.ProcessingLyrics(lyrics);
+        //}
+
+        /// <summary>
+        /// 把对象写入到<c>saveFolderPath</c>
+        /// </summary>
+        /// <param name="saveFolderPath">写入路径</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        //public void WriteFileTo(string saveFolderPath)
+        //{
+        //    if (saveFolderPath == null)
+        //        throw new ArgumentNullException(nameof(saveFolderPath));
+
+        //    string language;
+        //    if (_contentLanguage == null)
+        //    {
+        //        language = "Unknown";
+        //    }
+        //    else
+        //    {
+        //        language = _contentLanguage.ToString();
+        //    }
+        //    using (FileStream fileStream = new FileStream(saveFolderPath + $"\\{MusicName}-{language}.lrc", FileMode.Create))
+        //    {
+        //        using (StreamWriter file = new StreamWriter(fileStream))
+        //        {
+        //            foreach (var (timeTag, lyrics) in _data)
+        //            {
+        //                file.WriteLine($"{timeTag.ToTimeTag()}{lyrics}");
+        //            }
+        //        }
+        //    }
+        //}
+
+        /// <summary>
+        /// 移除<c>removeTime</c>之前的timeTag及lyrics
+        /// </summary>
+        /// <param name="removeTimeTag">移除此时间标签前的所有时间标签</param>
+        public void RemoveBefore(in TimeTag removeTimeTag)
+        {
+            for (var node = _data.First; node != null; node = node.Next)
+            {
+                if (node.Value.timeTag > removeTimeTag)
+                {
+                    node = node.Previous;
+                    _data.AddAfter(node, (removeTimeTag, node.Value.lyrics));
+                    //删除node及所有node前的数据
+                    for (LinkedListNode<(TimeTag timeTag, string lyrics)> previous; node != null; node = previous)
+                    {
+                        previous = node.Previous;
+                        _data.Remove(node);
+                    }
+                    //前移时间标签
+                    for (var needRemoveNode = _data.First; needRemoveNode != null; needRemoveNode = needRemoveNode.Next)
+                    {
+                        needRemoveNode.Value = (needRemoveNode.Value.timeTag - removeTimeTag, needRemoveNode.Value.lyrics);
+                    }
+                    break;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// 移除<c>removeTime</c>之后的timeTag及lyrics
+        /// </summary>
+        /// <param name="removeTimeTag">时间标签</param>
+        public void RemoveAfter(in TimeTag removeTimeTag)
+        {
+            for (var node = _data.First; node != null; node = node.Next)
+            {
+                if (node.Value.timeTag > removeTimeTag || node.Value.timeTag == removeTimeTag)
+                {
+                    _data.AddBefore(node, (removeTimeTag, END_FLAG));
+                    //删除node及所有node前的数据
+                    for (LinkedListNode<(TimeTag timeTag, string lyrics)> next; node != null; node = next)
+                    {
+                        //Console.WriteLine("移除" + node.Value);
+                        next = node.Next;
+                        _data.Remove(node);
+                    }
+                    break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 截取其中一段时间
+        /// </summary>
+        /// <param name="statr">开始</param>
+        /// <param name="end">结束</param>
+        /// <exception cref="ArgumentException">如果statr 大于 end</exception>
+        public void InterceptTime(in TimeTag statr, in TimeTag end)
+        {
+            if (statr < end)
+            {
+                throw new ArgumentException();
+            }
+            RemoveBefore(statr);
+            RemoveAfter(end);
+        }
+
+
+        //protected Dictionary<TimeTag, string> GetData()
+        //{
+        //    var map = new Dictionary<TimeTag, string>(data.Count);
+        //    foreach (var (timeTag, lyrics) in data)
+        //    {
+        //        map[timeTag] = lyrics;
+        //    }
+        //    return map;
+        //}
+
+        /// <summary>
+        /// 得到标准格式的LRC文件内容数组, 每个元素对应文件中的一行
+        /// </summary>
+        /// <returns>标准格式的LRC文件内容数组, 每个元素对应文件中的一行</returns>
+        public string[] GetLrcFileTypeArray()
+        {
+            List<string> list = new List<string>(_data.Count);
+
+            foreach (var (timeTag, lyrics) in _data)
+            {
+                list.Add(string.Format($"{timeTag.ToTimeTag()}{lyrics}"));
+            }
+            return list.ToArray();
+        }
+
+        ///// <summary>
+        ///// 歌词使用的语言
+        ///// </summary>
+        //public UnifiedLanguageCode? LyricsLanguage => _contentLanguage;
+
+        #region ICollection接口实现
+
+        public IEnumerator<(TimeTag, string)> GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            _data.Clear();
+        }
+
+        public bool Contains((TimeTag, string) item)
+        {
+            return _data.Contains(item);
+        }
+
+        public bool Remove((TimeTag, string) item)
+        {
+            return _data.Remove(item);
+        }
+
+        bool ICollection<(TimeTag, string)>.IsReadOnly => false;
+
+        void ICollection<(TimeTag, string)>.Add((TimeTag, string) item)
+        {
+            _data.AddLast(item);
+        }
+
+        void ICollection<(TimeTag, string)>.CopyTo((TimeTag, string)[] array, int arrayIndex)
+        {
+            _data.CopyTo(array, arrayIndex);
+        }
+        #endregion
+
+        object ICloneable.Clone()
+        {
+            (TimeTag, string)[] array = new (TimeTag, string)[_data.Count];
+            _data.CopyTo(array, 0);
+            var clone = new LyricsData(array);
+            return clone;
+        }
+
+        private LyricsData((TimeTag, string)[] datas)
+        {
+            foreach (var itme in datas)
+            {
+                _data.AddLast(itme);
+            }
+        }
+    }
+}
---
+++ BiliBiliSubtitlesTool/Lyric/Tools.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiliBiliSubtitlesTool.Lyric
+{
+    internal class Tools
+    {
+        /// <summary>
+        /// 传入一行带时间标签的歌词, 返回其歌词部分
+        /// </summary>
+        /// <param name="text">lrc文件中的一行歌词</param>
+        /// <returns>其歌词部分</returns>
+        public static string GetLineLyric(string text)
+        {
+            return text.Split(']')[1].Trim();
+        }
+
+        /// <summary>
+        /// 返回字符串, 其中包含了集合中的所有字符串,并用换行符分隔开, 会删除重复的字符串
+        /// </summary>
+        /// <param name="lyrics">LRC文件中的所有歌词, 不能带时间标签</param>
+        /// <returns>一个字符串, 其中包含了集合中的所有字符串,并用换行符分隔开</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string ProcessingLyrics(in string[] lyrics)
+        {
+            return ProcessingLyrics(new List<string>(lyrics));
+        }
+
+        /// <summary>
+        /// 返回字符串, 其中包含了集合中的所有字符串,并用换行符分隔开, 会删除重复的字符串
+        /// </summary>
+        /// <param name="lyrics">LRC文件中的所有歌词, 不能带时间标签</param>
+        /// <returns>一个字符串, 其中包含了集合中的所有字符串,并用换行符分隔开</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string ProcessingLyrics(List<string> lyrics)
+        {
+            if (lyrics == null)
+                throw new ArgumentNullException(nameof(lyrics));
+
+            //去除重复字符串, 以减少翻译字符数
+            lyrics = GetNonRepeatingElement(lyrics, out _);
+
+            StringBuilder sb = new StringBuilder(60);
+            for (int i = 0, max = lyrics.Count - 1; i < max; ++i)
+            {
+                sb.Append(lyrics[i]).Append('\n');
+            }
+            _ = sb.Append(lyrics[lyrics.Count - 1]);
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 得到一个没有重复元素的集合
+        /// </summary>
+        /// <param name="array">需要处理的集合</param>
+        /// <param name="repeatingElementsNumber">重复元素的数量</param>
+        /// <returns>一个<see cref="List{T}"/>,包含<c>array</c>中不重复的元素</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static List<string> GetNonRepeatingElement(string[] array, out uint repeatingElementsNumber)
+        {
+            if (array == null)
+                throw new ArgumentNullException(nameof(array));
+
+            var newArray = new List<string>(array).Distinct().ToList();
+            repeatingElementsNumber = (uint)(array.Length - newArray.Count);
+            return newArray;
+        }
+
+        /// <summary>
+        /// 得到一个没有重复元素的集合
+        /// </summary>
+        /// <param name="array">需要处理的集合</param>
+        /// <param name="repeatingElementsNumber">重复元素的数量</param>
+        /// <returns>一个<see cref="List{T}"/>,包含<c>array</c>中不重复的元素</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static List<string> GetNonRepeatingElement(List<string> array, out uint repeatingElementsNumber)
+        {
+            return GetNonRepeatingElement(array.ToArray(), out repeatingElementsNumber);
+        }
+
+        /// <summary>
+        /// 把<c>sourceText</c>中的歌词部分替换成<c>replacedText</c>
+        /// </summary>
+        /// <param name="sourceText">标准歌词</param>
+        /// <param name="replacedText">要替换的字符串</param>
+        /// <returns>被替换后的字符串</returns>
+        /// <exception cref="ArgumentNullException">
+        /// 如果<c>sourceText</c>或
+        /// <c>replacedText</c>为null       
+        /// </exception>
+        public static string ReplaceLyric(string sourceText, string replacedText)
+        {
+            if (sourceText == null)
+            {
+                throw new ArgumentNullException(nameof(sourceText));
+            }
+            if (replacedText == null)
+            {
+                throw new ArgumentNullException(nameof(replacedText));
+            }
+            string result = sourceText.Split(']')[0];
+            return result + ']' + replacedText;
+        }
+    }
+}
+}
